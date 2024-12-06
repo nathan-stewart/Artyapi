9bcc74c49699ec91cead5261ff1cd863e6ef0ed5
Nathan Stewart <nathan-stewart@users.noreply.github.com>
2024-12-06 09:12:59 -0500

    matplot starting to work

--- Plotting.py
+++ Plotting.py
@@ -1,18 +1,20 @@
-#!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 import matplotlib.colors as mcolors
 import time
-import cProfile
-import pstats
-from util import * 
+from math import ceil
 
 LOGMIN = 10**(-96/20)
 LOGMAX = 10**(12/20)
 
+def format_hz(x):
+    if x >= 1000:
+        return f'{x/1000:.1f}kHz'
+    else:
+        return f'{x}Hz'
+
 class Plotting:
-    def __init__(self, width=1920, height=480, f0 = 40, f1 = 20e3):
+    def __init__(self, width=1920, height=480, f0=40, f1=20e3):
         self.dpi = 100
         self.FFT_BINS = width
         self.FFT_HISTORY_LENGTH = height
@@ -20,79 +22,63 @@ class Plotting:
         self.RMS_HEIGHT = height
         self.fft_data = np.zeros((self.FFT_HISTORY_LENGTH, self.FFT_BINS, 3))  # RGB channels
         self.rms_data = np.zeros((self.RMS_LENGTH, self.RMS_HEIGHT))
-            
-        # Create figure and axes
-        self.fig, self.ax_fft = plt.subplots(figsize=(width / self.dpi, height / self.dpi))
-        self.fig_rms, self.ax_rms = plt.subplots(figsize=(width / self.dpi, height / self.dpi))
-
-        # Initialize the image for FFT display
-        self.im = self.ax_fft.imshow(self.fft_data, aspect='auto', interpolation='none', norm=mcolors.Normalize(vmin=0, vmax=1))
+        self.latest_rms = np.zeros(self.RMS_HEIGHT)
+        self.latest_fft = np.zeros((self.FFT_BINS, 3))
         
-        # Set titles and labels
+        # Create figure and axes for FFT display
+        self.fig_fft, self.ax_fft = plt.subplots(figsize=(width / self.dpi, height / self.dpi))
+        self.im_fft = self.ax_fft.imshow(self.fft_data, aspect='auto', interpolation='none', norm=mcolors.Normalize(vmin=0, vmax=1))
         self.ax_fft.set_title('FFT Display')
-        self.ax_fft.set_xlabel('Frequency')
-        
-        # Set x-axis properties
         self.ax_fft.xaxis.set_label_position('top')
-        ticks = [int(round(f0 * 2 ** (i/3),0)) for i in range(27)]
+        octaves = ceil(np.log2(f1 / f0))
+        ticks = [int(round(f0 * 2 ** (i/3), 0)) for i in range(3*octaves)]
         if f1 not in ticks:
             ticks.append(int(f1))
-
         self.ax_fft.set_xscale('log', base=2)
         self.ax_fft.set_xlim(f0, f1)
         self.ax_fft.set_xticks(ticks)
         self.ax_fft.xaxis.tick_top()
         self.ax_fft.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{format_hz(x)}'))
-        
         self.ax_fft.yaxis.set_visible(False)
+
+        # Create figure and axes for RMS display
+        self.fig_rms, self.ax_rms = plt.subplots(figsize=(width / self.dpi, height / self.dpi))
+        self.im_rms = self.ax_rms.imshow(self.rms_data, aspect='auto', interpolation='none', norm=mcolors.Normalize(vmin=0, vmax=1))
+        self.ax_rms.set_title('SPL')
+        self.ax_rms.set_ylim(-96, 12)
 
         # Frame rate counter
         self.frame_count = 0
         self.start_time = time.time()
 
-    def update(self, fft):
-        # Update FFT data
-        self.fft_data = np.roll(self.fft_data, 1, axis=0)
-        self.fft_data[0] = fft
-
-        # Update the image data
-        self.im.set_data(self.fft_data)
-
-        return [self.im]
-
-    def update_rms(self, rms):
+    def update_data(self, rms, fft):
         # Update RMS data
         self.rms_data = np.roll(self.rms_data, 1, axis=0)
         self.rms_data[0] = rms
-
-        # Update the image data
         self.im_rms.set_data(self.rms_data)
 
-        return [self.im_rms]
-    
-    def start(self, datatype='fft'):
-        if datatype == 'fft':
-            self.ani = animation.FuncAnimation(self.fig, self.update, interval=33, blit=True)
-        elif datatype == 'rms':
-            self.ani = animation.FuncAnimation(self.fig_rms, self.update_rms, interval=33, blit=True)
-        plt.tight_layout()
-        plt.show()
+        # Update FFT data
+        self.fft_data = np.roll(self.fft_data, 1, axis=0)
+        self.fft_data[0] = fft
+        self.im_fft.set_data(self.fft_data)
+        plt.pause(0.001)
+        return [self.im_fft]
+
+    def show(self):
+        plt.show(block=False)
 
 # Example usage
 if __name__ == "__main__":
     plotter = Plotting()
 
-    test_ftt = np.random.rand(480, 1920, 3)
-    test_rms = np.random.rand(1920, 480)
-
     # Profiler
     profiler = cProfile.Profile()
     profiler.enable()
 
-    plotter.start('fft')
+    plotter.start_both()
     
     profiler.disable()
     with open('profile_output.txt', 'w') as f:
-        ps = pstats.Stats(self.profiler, stream=f)
+        ps = pstats.Stats(profiler, stream=f)
         ps.strip_dirs().sort_stats('cumulative').print_stats(20)
     print("Profiling data saved to 'profile_output.txt'")
